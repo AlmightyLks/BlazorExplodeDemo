5307eeefc4c101fe9116eb5da1b4b08db497600d
AlmightyLks <almightylks@protonmail.com>
2022-12-03 14:39:48 +0100

    init

---
+++ BlazorExplodeDemo/DownloadService.cs
@@ -0,0 +1,20 @@
+﻿using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace BlazorExplodeDemo;
+
+public class DownloadService
+{
+    public async Task<(Stream Stream, string Name)> DownloadAudioAsync(string url)
+    {
+        var youtube = new YoutubeClient();
+
+        var video = await youtube.Videos.GetAsync(url);
+
+        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+        var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+        var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+
+        return (stream, $"{video.Title}.mp3");
+    }
+}
---
+++ BlazorExplodeDemo/Pages/Index.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorExplodeDemo.Pages;
+
+public partial class Index
+{
+    [Inject]
+    public DownloadService DownloadService { get; set; } = null!;
+
+    private string? videoUrl;
+
+    private async Task DownloadFileFromStream()
+    {
+        if (String.IsNullOrWhiteSpace(videoUrl))
+            return;
+
+        (var fileStream, var fileName) = await DownloadService.DownloadAudioAsync(videoUrl);
+
+        using var streamRef = new DotNetStreamReference(stream: fileStream);
+        await JS.InvokeVoidAsync("downloadFileFromStream", fileName, streamRef);
+    }
+}
